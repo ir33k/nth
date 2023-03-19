80d0b9fa03a12fed6eaa3f12d38292dd43f7464d
irek <irek@gabr.pl>
2023-03-19 13:12:26 +0100

    Return non zero error code on error, add help

--- nth.c
+++ nth.c
@@ -1,11 +1,11 @@
-/* Print NTH line from stream, negative argument makes it print from end. */
-/* In case of negative argument program can print line of max BUFSIZ length. */
+/* Print NTH line from stream. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* Print NTH line from the beginning of the FP stream. */
-void
+/* Print NTH line from the beginning of the FP stream.
+ * Return non zero value when nothing was printed. */
+int
 from_beg(FILE *fp, int nth)
 {
 	int c;
@@ -16,20 +16,22 @@ from_beg(FILE *fp, int nth)
 		}
 	}
 	if (c == EOF) {
-		return;
+		return 1;
 	}
 	while ((c = fgetc(fp)) != EOF && c != '\n') {
 		/* TODO(irek): IDK what to do in case of putchar error. */
 		putchar(c);
 	}
 	putchar('\n');
+	return 0;
 }
 
-/* Print NTH line from the end of the FP stream. */
-void
+/* Print NTH line from the end of the FP stream.
+ * Return non zero value when nothing was printed. */
+int
 from_end(FILE *fp, int nth)
 {
-	int i;
+	int i, res = 1;
 	char **lines = malloc(sizeof(char*)*nth);
 	for (i = 0; i < nth; i++) {
 		lines[i] = malloc(BUFSIZ);
@@ -41,33 +43,54 @@ from_end(FILE *fp, int nth)
 	if (i >= nth) {
 		/* TODO(irek): IDK what to do in case of fputs error. */
 		fputs(lines[(i-nth)%nth], stdout);
+		res = 0;
 	}
 	free(lines);
+	return res;
 }
 
 int
 main(int argc, char **argv)
 {
-	int argv1;
+	int argv1 = 0, res;
 	FILE *fp = stdin;
-	if (argc < 2 || (argv1 = atoi(argv[1])) == 0) {
-		argv1 = 1; /* Default when no arg or invalid arg */
+	if (argc > 1 &&
+	    (!strncmp(argv[1], "--h", 3) ||
+	     !strncmp(argv[1],  "-h", 2))) {
+		fprintf(stderr,
+			"usage: %s [[-][+]n] [file]\n\n"
+			"\tn\tLine number to print, default to -1.\n"
+			"\t\tNegative number prints from bottom.\n"
+			"\tfile\tInput file, default to stdin.\n\n"
+			"examples:\n\n"
+			"\t$ cat input | nth     # Print last line\n"
+			"\t$ tail input | nth 2  # Print second line\n"
+			"\t$ nth -3 input        # Third from the bottom\n",
+			argv[0]);
+		return 1;
 	}
-	if (argc > 2 && (fp = fopen(argv[argc-1], "r")) == NULL) {
+	if (argc > 2) {
+		fp = fopen(argv[argc-1], "r");
+	}
+	if (argc > 1) {
+		argv1 = atoi(argv[1]);
+		if (fp == stdin && argv1 == 0) {
+			fp = fopen(argv[argc-1], "r");
+		}
+	}
+	if (fp == NULL) {
 		perror("fopen");
 		return 1;
 	}
-	/* Searching from the top is totally different than from the bottom.
-	 * First is simple, second requires dynamic memory allocation.
-	 * So this is condition that will decide the faith of your memory. */
-	if (argv1 > 0) {
-		from_beg(fp, argv1);
-	} else {
-		from_end(fp, argv1*-1); /* Make argv1 positive. */
+	if (argv1 == 0) {
+		argv1 = -1; /* By default print last line. */
 	}
+	/* Searching from the top is totally different than from the bottom.
+	 * Only second requires dynamic memory allocation. */
+	res = argv1 > 0 ? from_beg(fp, argv1) : from_end(fp, argv1*-1);
 	if (fp != stdin && fclose(fp) == EOF) {
 		perror("fclose");
 		return 1;
 	}
-	return 0;
+	return res;
 }
