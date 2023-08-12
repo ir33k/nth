5f62cc43aff0be5c632946cf9f5b131a139572af
irek <irek@gabr.pl>
2023-08-12 22:40:39 +0200

    Add more tests

--- nth.t.c
+++ nth.t.c
@@ -2,8 +2,8 @@
 
 TEST("Should print help message")
 {
-	RUN("./nth -h",     0, "snap/help", "snap/empty", 0);
-	RUN("./nth -help",  0, "snap/help", "snap/empty", 0);
+	RUN("./nth  -h",    0, "snap/help", "snap/empty", 0);
+	RUN("./nth  -help", 0, "snap/help", "snap/empty", 0);
 	RUN("./nth --help", 0, "snap/help", "snap/empty", 0);
 	RUN("./nth --h",    0, "snap/help", "snap/empty", 0);
 }
@@ -33,8 +33,26 @@ TEST("Should print last line by default")
 	RUN("./nth", "snap/input", "snap/input15", "snap/empty", 0);
 }
 
+TEST("Should ignore invalid line number")
+{
+	/* It will think that "0" is a file path. */
+	SRUN("./nth 0",   "Lorem ipsum", "", "fopen: No such file or directory\n", 1);
+	SRUN("./nth aaa", "Lorem ipsum", "", "fopen: No such file or directory\n", 1);
+	/* Last arg is taken as file path so in this case 0 is
+	 * ignored, it should work like default case giving last
+	 * line. */
+	RUN("./nth 0   snap/input", 0, "snap/input15", "snap/empty", 0);
+	RUN("./nth aaa snap/input", 0, "snap/input15", "snap/empty", 0);
+}
+
 TEST("Should print nothing when line number is too big")
 {
 	RUN("./nth  20", "snap/input", "snap/empty", "snap/empty", 1);
 	RUN("./nth -20", "snap/input", "snap/empty", "snap/empty", 1);
+}
+
+TEST("Should return nothing when input is missing")
+{
+	RUN("./nth", "snap/empty", "snap/empty", "snap/empty", 1);
+	RUN("./nth", 0,            "snap/empty", "snap/empty", 1);
 }
