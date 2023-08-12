a4e9d5443e8c91bd7440d9f6340922a79ceffcf3
irek <irek@gabr.pl>
2023-08-12 22:29:30 +0200

    Update tests

--- nth.t.c
+++ nth.t.c
@@ -10,31 +10,31 @@ TEST("Should print help message")
 
 TEST("Should print lines from top")
 {
-	RUN("./nth  1", "snap/input", "snap/input01", 0, 0);
-	RUN("./nth  5", "snap/input", "snap/input05", 0, 0);
-	RUN("./nth 15", "snap/input", "snap/input15", 0, 0);
+	RUN("./nth  1", "snap/input", "snap/input01", "snap/empty", 0);
+	RUN("./nth  5", "snap/input", "snap/input05", "snap/empty", 0);
+	RUN("./nth 15", "snap/input", "snap/input15", "snap/empty", 0);
 }
 
 TEST("Should print lines from bottom")
 {
-	RUN("./nth  -1", "snap/input", "snap/input15", 0, 0);
-	RUN("./nth -11", "snap/input", "snap/input05", 0, 0);
-	RUN("./nth -15", "snap/input", "snap/input01", 0, 0);
+	RUN("./nth  -1", "snap/input", "snap/input15", "snap/empty", 0);
+	RUN("./nth -11", "snap/input", "snap/input05", "snap/empty", 0);
+	RUN("./nth -15", "snap/input", "snap/input01", "snap/empty", 0);
 }
 
 TEST("Should work with stdin and input file")
 {
-	RUN("./nth 1", "snap/input", "snap/input01", 0, 0);
-	RUN("./nth 1 snap/input", 0, "snap/input01", 0, 0);
+	RUN("./nth 1", "snap/input", "snap/input01", "snap/empty", 0);
+	RUN("./nth 1 snap/input", 0, "snap/input01", "snap/empty", 0);
 }
 
 TEST("Should print last line by default")
 {
-	RUN("./nth", "snap/input", "snap/input15", 0, 0);
+	RUN("./nth", "snap/input", "snap/input15", "snap/empty", 0);
 }
 
 TEST("Should print nothing when line number is too big")
 {
-	RUN("./nth  20", "snap/input", "snap/empty", 0, 1);
-	RUN("./nth -20", "snap/input", "snap/empty", 0, 1);
+	RUN("./nth  20", "snap/input", "snap/empty", "snap/empty", 1);
+	RUN("./nth -20", "snap/input", "snap/empty", "snap/empty", 1);
 }
